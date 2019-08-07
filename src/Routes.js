96295bec28a0bf56bfbfcbd8aeb7fc75647179e9
aspenjames <aspenjames@tqca.org>
2019-08-07 16:40:10 -0700

    add About

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import About from './components/About';
+
 function Routes() {
 	return (
 		<>
-			<Route exact path='/' render={() => <div>About</div>} />
+			<Route exact path='/' component={About} />
 			<Route exact path='/projects' render={() => <div>Projects</div>} />
 			<Route exact path='/contact' render={() => <div>Contact</div>} />
 		</>
---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+	return (
+		<div className='container'>
+			<h1 className='mainHeading'>Aspen James</h1>
+			<h2 className='subHeading'>Software Developer, Linux Lover, Tree Hugger</h2>
+
+			<p>Full-stack developer specializing in Ruby, React, and Redux. I enjoy lightweight applications, databases, and finding new ways to organize and optimize tasks. My background is in coffee, customer service, and training, which has helped me cultivate a strong attention to detail and effective interpersonal skills. My natural curiosity and interest in continuous learning have driven me in the pursuit of ongoing advancement of my programming education. In my free time, I enjoy learning new programming languages, finding (or building) terminal-based alternatives for graphical applications, hiking, and tinkering with various computer hardware and operating systems.</p>
+		</div>
+	);
+}
+
+export default About;
