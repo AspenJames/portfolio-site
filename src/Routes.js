5e4a7c22af797f7e004178b20e9170f3f1ade622
aspenjames <aspenjames@tqca.org>
2019-08-07 17:54:03 -0700

    add projects placeholder

--- src/Routes.js
+++ src/Routes.js
@@ -9,7 +9,7 @@ function Routes() {
 	return (
 		<>
 			<Route exact path='/' component={About} />
-			<Route exact path='/projects' render={() => <div>Projects</div>} />
+			<Route exact path='/projects' component={Projects} />
 			<Route exact path='/contact' component={Contact} />
 		</>
 	);
---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Projects() {
+	return (
+		<div className='container'>
+			<h1 className='mainHeading'>Under Development...</h1>
+			<h2 className='subHeading'>Check back later for some examples of my work!</h2>
+		</div>
+	);
+}
+
+export default Projects;
