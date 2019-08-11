10ff40a25930da070f870ad5fecbd8d4ee040413
Aspen James <aspenjames@tqca.org>
2019-08-11 11:04:46 -0700

    hamburger collapse

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { profilePhoto } from '../photoSrc';
 
 function NavBar() {
+	const [iconClass, setIconClass] = useState('fa fa-bars');
+
 	const navRef = React.createRef();
 
 	const handleClick = e => {
 		if (!navRef.current.classList.contains('responsive')) {
 			navRef.current.classList.add('responsive');
+			setIconClass('fa fa-times');
 		} else {
 			navRef.current.classList.remove('responsive');
+			setIconClass('fa fa-bars');
 		}
 	}
 
 	const handleNav = _ => {
 		if (navRef.current.classList.contains('responsive')) {
 			navRef.current.classList.remove('responsive');
+			setIconClass('fa fa-bars');
 		}
 	}
 
@@ -34,7 +39,7 @@ function NavBar() {
 				onClick={handleNav}
 				activeClassName='active'
 				className='navLink'>Contact</NavLink>
-			<i className='fa fa-bars' onClick={handleClick} />
+			<i className={iconClass} onClick={handleClick} />
 		</div>
 	);
 }
